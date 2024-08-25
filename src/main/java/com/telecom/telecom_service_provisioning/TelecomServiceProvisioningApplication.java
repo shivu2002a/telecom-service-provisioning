396113eecf649c6210e7e45c4e1df00a4729d1f2
shivu2002a <shivu.a.1945@gmail.com>
2024-08-25 13:54:44 +0530

    Adding initial project

---
+++ src/main/java/com/telecom/telecom_service_provisioning/TelecomServiceProvisioningApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.telecom_service_provisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomServiceProvisioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomServiceProvisioningApplication.class, args);
+	}
+
+}
