3ac075a9c9406f4912e347cba35a8e309614ae72
shivu2002a <shivu.a.1945@gmail.com>
2024-08-26 19:40:29 +0530

    Adding basic project structure and sql script

---
+++ dbscript.sql
@@ -0,0 +1,69 @@
+
+-- Table to store user information
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Password varchar(50),
+    phoneno varchar(10),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table to store information about different internet services
+CREATE TABLE InternetServices (
+    ServiceID INT PRIMARY KEY AUTO_INCREMENT,
+    ServiceName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    serviceType varchar(50),
+    serviceDownloadSpeed INT,
+    serviceUploadSpeed INT,
+    benefits TEXT,
+    criteria text,
+    active boolean,
+    MonthlyCost DECIMAL(10, 2)
+);
+
+-- Table to store information about different TV services
+CREATE TABLE TvServices (
+    ServiceID INT PRIMARY KEY AUTO_INCREMENT,
+    ServiceName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    benefits text,
+    criteria text,
+    active boolean,
+    MonthlyCost DECIMAL(10, 2)
+);
+
+-- Table to link users with the TV services they have availed
+CREATE TABLE TvServicesAvailed (
+    UserID INT,
+    ServiceID INT,
+    StartDate DATE,
+    EndDate DATE,
+    PRIMARY KEY (UserID, ServiceID, StartDate),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ServiceID) REFERENCES TvServices(ServiceID)
+);
+
+-- Table to link users with the internet services they have availed
+CREATE TABLE InternetServicesAvailed (
+    UserID INT,
+    ServiceID INT,
+    StartDate DATE,
+    EndDate DATE,
+    PRIMARY KEY (UserID, ServiceID, StartDate),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ServiceID) REFERENCES InternetServices(ServiceID)
+);
+
+CREATE TABLE PendingRequests (
+    requestID INT,
+    ServiceID INT,
+    userID INT,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    serviceType VARCHAR(20),
+    reuestStatus VARCHAR(15),
+    remarks VARCHAR(100)
+)
+
---
+++ src/main/java/com/telecom/telecom_service_provisioning/model/InternetServiceAvailed.java
@@ -0,0 +1,10 @@
+package com.telecom.telecom_service_provisioning.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class InternetServiceAvailed {
+    
+}
